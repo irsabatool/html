b1b7626bfc474d3a09cfc43c42aa03dd3760a9b7
irsabatool <batoolirsa8@gmail.com>
2026-03-29 19:01:18 +0500

    Add files via upload

---
+++ print.py
@@ -0,0 +1,12 @@
+# Printing a greeting message 
+print("hello,World")
+
+#printing my name
+print("my name is irsa")
+
+#printing a fun fact about myself 
+#print("i love playing football")
+print("i love playing football")
+
+#performing and printing a simple calculation 
+print("the result is 3+5 is :" , 3+5)
